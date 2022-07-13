61658492d6c39c3899290e2a56972610e6275335
jackcho <jackcho@wustl.edu>
2022-07-13 19:27:12 +0900

    boilerplate (before connecting to db)

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,21 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+const tasks = require('./routes/tasks')
+
+
+//middleware
+app.use(express.json()) // access data in req.body
+
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks) //base route
+
+
+const port = 3000
+
+app.listen(port, console.log(`Server is listening on port${port}...`))
