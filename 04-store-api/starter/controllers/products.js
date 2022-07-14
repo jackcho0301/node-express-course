f9001f8c6687bbf9588c782653c2985405329a9b
jackcho <jackcho@wustl.edu>
2022-07-14 15:17:16 +0900

    controller basic filter from param

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,20 +2,30 @@ const { query } = require('express')
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
+    const search = 'ab'
     const products = await Product.find({
-        name: 'vast table'
+        name: {$regex: search, $options: 'i'}
     })
-    res.status(200).json({products})
+    res.status(200).json({products, numHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-    const {featured} = req.query //pull out only the property we need, to avoid bug
+    const {featured, company, name} = req.query //pull out only the property we need, to avoid bug
     
     const queryObject = {} //create new queryObject.
 
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
     }
+
+    if (company) {
+        queryObject.company = company
+    }
+
+    if (name) {
+        queryObject.name = {$regex: name, $options: 'i'} // i means 'case insensitive
+    }
+
     console.log(queryObject)
     const products = await Product.find(queryObject)
     res.status(200).json({products, numHits: products.length})
