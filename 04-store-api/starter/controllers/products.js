ce01d8e437a62854db466c75cbe7247502304151
jackcho <jackcho@wustl.edu>
2022-07-14 15:43:52 +0900

    fields (select)

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,12 +2,12 @@ const { query } = require('express')
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).sort('-name price')
+    const products = await Product.find({}).select('name price')
     res.status(200).json({products, numHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-    const {featured, company, name, sort} = req.query //pull out only the property we need, to avoid bug
+    const {featured, company, name, sort, fields} = req.query //pull out only the property we need, to avoid bug
     
     const queryObject = {} //create new queryObject.
 
@@ -34,6 +34,11 @@ const getAllProducts = async (req, res) => {
     else {
         //default sorting
         result = result.sort('createdAt')
+    }
+
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList )
     }
 
     const products = await result
