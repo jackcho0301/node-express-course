97f45ae701f1160edd78ce6f06bd2726ab9d281a
jackcho <jackcho@wustl.edu>
2022-07-14 12:55:30 +0900

    basic setup (controller, route, db)

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,43 @@
-console.log('04 Store API')
+require('dotenv').config()
+//async errors
+
+const express = require('express')
+const app = express()
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+
+//middleware
+app.use(express.json())
+
+
+
+//routes
+app.get('/', (req, res)=> {
+    res.send('<h1>Store API</h1><a href="api/v1/products">products route</a>')
+})
+
+//products route
+app.use('/api/v1/products', productsRouter)
+
+
+app.use(notFoundMiddleware) //404
+app.use(errorMiddleware) //500
+
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening at port: ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
