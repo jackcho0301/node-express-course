ff7f8b0bb4d75eccd991789d6e30cbb0340c6b8a
jackcho <jackcho@wustl.edu>
2022-07-14 15:05:04 +0900

    create queryObject

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,9 +1,24 @@
+const { query } = require('express')
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({msg: 'products testing route'})
+    const products = await Product.find({
+        name: 'vast table'
+    })
+    res.status(200).json({products})
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products route'})
+    const {featured} = req.query //pull out only the property we need, to avoid bug
+    
+    const queryObject = {} //create new queryObject.
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
+    res.status(200).json({products, numHits: products.length})
 }
 
 module.exports = {
