5ae66066afe114bcd93d87f0f2892248eb6cd6db
jackcho <jackcho@wustl.edu>
2022-07-15 19:19:11 +0900

    password hashing refactored (pre middleware)

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,21 +1,12 @@
 const User = require('../models/User')
-const {StatusCodes} = require('http-status-codes')
-const {BadRequestError} = require('../errors')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
 const bcrypt = require('bcryptjs') //for hashing
 
 const register = async (req, res) => {
 
-    const {name, email, password} = req.body
-    // if (!name || !email || !password) {
-    //     throw new BadRequestError('Please provide name, email and password')
-    // }
-
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-
-    const tempUser = {name, email, password: hashedPassword}
-
-    const user = await User.create({...tempUser})
+    //password hashing is done in User model
+    const user = await User.create({ ...req.body })
     res.status(StatusCodes.CREATED).json(user)
 }
 
@@ -24,6 +15,6 @@ const login = async (req, res) => {
 }
 
 module.exports = {
-    register, 
+    register,
     login
 }
--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs') //for hashing
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -19,6 +20,13 @@ const UserSchema = new mongoose.Schema({
         minlength: 6,
     },
 })
+
+//mongoose documentation: 'pre middleware functions are executed one after another, when each middleware calls next'
+UserSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt) //'this' refers to the document
+    next()
+} )
 
 
 module.exports = mongoose.model('User', UserSchema)
