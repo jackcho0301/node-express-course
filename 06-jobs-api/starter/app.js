5485a4f109b0ef47c0b6b4c719b94b910ccbca81
jackcho <jackcho@wustl.edu>
2022-07-17 13:25:28 +0900

    place auth middleware for JobRoutes

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -7,6 +7,8 @@ const app = express();
 //connectDB
 const connectDB = require('./db/connect')
 
+const authenticateUser = require('./middleware/authentication')
+
 //routers
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
@@ -22,7 +24,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobsRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter) // auth middleware placed for all the routes in jobsRouter. Necessary because all routes in JobsRouter must be protected
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
